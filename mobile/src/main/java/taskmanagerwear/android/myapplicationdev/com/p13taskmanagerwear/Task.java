b2064f4fc4b9b022ea3478206c329caa17c577e0
KwonYiJun <15031795@myrp.edu.sg>
2017-08-15 21:19:34 +0800

    Initial commit

---
+++ mobile/src/main/java/taskmanagerwear/android/myapplicationdev/com/p13taskmanagerwear/Task.java
@@ -0,0 +1,43 @@
+package taskmanagerwear.android.myapplicationdev.com.p13taskmanagerwear;
+
+import java.io.Serializable;
+
+/**
+ * Created by 15031795 on 15/8/2017.
+ */
+public class Task implements Serializable {
+
+    private int id;
+    private String name, description;
+
+    public Task(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return id + " " + name + "\n" + description;
+    }
+}
